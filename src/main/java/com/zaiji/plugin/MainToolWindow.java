8604707dece6694ba04eadffc600d68db01e7ab1
zaiji <710754933@qq.com>
2021-12-15 09:45:03 +0800

    代码提交，布局样式修改

--- src/main/java/com/zaiji/plugin/MainToolWindow.java
+++ src/main/java/com/zaiji/plugin/MainToolWindow.java
@@ -3,6 +3,7 @@ package com.zaiji.plugin;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
+import com.intellij.ui.components.JBScrollPane;
 import com.intellij.ui.content.Content;
 import com.intellij.ui.content.ContentFactory;
 import com.zaiji.plugin.maven.MvnInstallGeneratorToolBox;
@@ -10,8 +11,6 @@ import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map;
@@ -32,21 +31,24 @@ public class MainToolWindow implements ToolWindowFactory {
     /**
      * 主工具箱布局
      */
-    private class MainToolWindowLayOut {
+    private static class MainToolWindowLayOut {
 
-        private JPanel mainJPanel;
-        private Box leftMenuJPanel;
-        private JPanel rightContentJPanel;
-        private LinkedList<JPanel> panelList = new LinkedList();
+        private final JPanel mainJpanel;
+        private final Box leftMenuJpanel;
+        private final JPanel rightContentJpanel;
+        private final LinkedList<JPanel> panelList = new LinkedList<>();
 
         public MainToolWindowLayOut() {
 
-            mainJPanel = new JPanel();
-            mainJPanel.setLayout(new BorderLayout());
-            leftMenuJPanel = Box.createVerticalBox();
-            rightContentJPanel = new JPanel();
-            mainJPanel.add(leftMenuJPanel, BorderLayout.WEST);
-            mainJPanel.add(rightContentJPanel, BorderLayout.CENTER);
+            mainJpanel = new JPanel();
+            mainJpanel.setLayout(new BorderLayout());
+            leftMenuJpanel = Box.createVerticalBox();
+            final JBScrollPane jbScrollPane1 = new JBScrollPane(leftMenuJpanel);
+            rightContentJpanel = new JPanel();
+            rightContentJpanel.setLayout(new BorderLayout());
+            final JBScrollPane jbScrollPane2 = new JBScrollPane(rightContentJpanel);
+            mainJpanel.add(jbScrollPane1, BorderLayout.WEST);
+            mainJpanel.add(jbScrollPane2, BorderLayout.CENTER);
             init();
         }
 
@@ -60,26 +62,24 @@ public class MainToolWindow implements ToolWindowFactory {
                     final Box horizontalBox = Box.createHorizontalBox();
                     horizontalBox.add(jButton);
                     panelList.add(content);
-                    jButton.addActionListener(new ActionListener() {
-                        @Override
-                        public void actionPerformed(ActionEvent aaa) {
-                            panelList.forEach(e -> e.setVisible(false));
-                            content.setVisible(true);
-                        }
+                    jButton.addActionListener(aaa -> {
+                        panelList.forEach(e -> e.setVisible(false));
+                        content.setVisible(true);
                     });
 
-                    leftMenuJPanel.add(horizontalBox);
-                    rightContentJPanel.add(content);
+                    leftMenuJpanel.add(horizontalBox);
+                    rightContentJpanel.add(content, BorderLayout.CENTER);
                 } catch (Exception e) {
                     System.out.println("组件：【" + key + "】初始化失败！" + e.getMessage());
+                    e.printStackTrace();
                 }
             });
-            leftMenuJPanel.updateUI();
-            rightContentJPanel.updateUI();
+            leftMenuJpanel.updateUI();
+            rightContentJpanel.updateUI();
         }
 
         public JPanel getContent() {
-            return mainJPanel;
+            return mainJpanel;
         }
     }
 
