a1d66f513406030ab3d7abbb9155c34d78618c65
zaiji <710754933@qq.com>
2022-01-25 20:17:30 +0800

    代码提交： 1、日历插件优化

--- src/main/java/com/zaiji/plugin/date/DateUtil.java
+++ src/main/java/com/zaiji/plugin/date/DateUtil.java
@@ -4,6 +4,9 @@ import com.nlf.calendar.Lunar;
 import com.zaiji.annotation.PluginComponentInfo;
 
 import javax.swing.*;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyledDocument;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.*;
@@ -30,13 +33,18 @@ public class DateUtil {
     private JTabbedPane tabbedPane1;
     private JLabel nowDateTimeTextLabel;
     private JPanel mainPane;
-    private JLabel lunarCalendarTextLabel;
+    private JTextPane lunarTextPane;
 
     //定时任务
-    final ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(1);
+    final ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(2);
 
     {
         try {
+            //textpane文本居中
+            StyledDocument doc = lunarTextPane.getStyledDocument();
+            SimpleAttributeSet center = new SimpleAttributeSet();
+            StyleConstants.setAlignment(center, StyleConstants.ALIGN_CENTER);
+            doc.setParagraphAttributes(0, doc.getLength(), center, false);
 
             //时间定时任务
             scheduledExecutorService.scheduleAtFixedRate(() -> {
@@ -44,9 +52,37 @@ public class DateUtil {
                 nowDateTimeTextLabel.setText(printTime);
             }, 0, 1000, TimeUnit.MILLISECONDS);
 
-            //农历定时任务
-            final Lunar lunar = Lunar.fromDate(new Date());
-            lunarCalendarTextLabel.setText(lunar.toFullString());
+            scheduledExecutorService.scheduleAtFixedRate(() -> {
+                //农历定时任务
+                final Lunar lunar = Lunar.fromDate(new Date());
+                StringBuilder s = new StringBuilder();
+                s.append(lunar);
+                s.append("\r\n");
+                s.append(lunar.getYearInGanZhi());
+                s.append("(");
+                s.append(lunar.getYearShengXiao());
+                s.append(")年 ");
+                s.append(lunar.getMonthInGanZhi());
+                s.append("(");
+                s.append(lunar.getMonthShengXiao());
+                s.append(")月 ");
+                s.append(lunar.getDayInGanZhi());
+                s.append("(");
+                s.append(lunar.getDayShengXiao());
+                s.append(")日 ");
+                s.append(lunar.getTimeZhi());
+                s.append("(");
+                s.append(lunar.getTimeShengXiao());
+                s.append(")时");
+                s.append("\r\n");
+                s.append("星期");
+                s.append(lunar.getWeekInChinese());
+
+
+                lunarTextPane.setText(s.toString());
+            }, 0, 1000, TimeUnit.MILLISECONDS);
+
+
         } catch (Exception e) {
             e.printStackTrace();
         }
