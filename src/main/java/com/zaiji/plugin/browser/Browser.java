3bdc2727fe2153c0f89c13581ee10fa93bf8750b
zaiji <710754933@qq.com>
2022-03-30 14:45:46 +0800

    fix
    - 修复了浏览器相关的gradle依赖
    - 修复了浏览器相关代码，取消了浏览器功能注册（待优化）

--- src/main/java/com/zaiji/plugin/browser/Browser.java
+++ src/main/java/com/zaiji/plugin/browser/Browser.java
@@ -1,11 +1,11 @@
 package com.zaiji.plugin.browser;
 
-//import com.teamdev.jxbrowser.engine.Engine;
 import com.zaiji.annotation.PluginComponentInfo;
+import chrriis.dj.nativeswing.swtimpl.NativeInterface;
+import chrriis.dj.nativeswing.swtimpl.components.JWebBrowser;
 
 import javax.swing.*;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+import java.awt.*;
 
 /**
  * 浏览器
@@ -13,61 +13,37 @@ import java.awt.event.WindowEvent;
  * @author zaiji
  */
 
-@PluginComponentInfo(name = "浏览器")
+//@PluginComponentInfo(name = "浏览器")
 public class Browser {
     private JPanel mainPane;
 
     {
-//        UIUtils.setPreferredLookAndFeel();
-//        NativeInterface.open();
-//        mainPane.setLayout(new BorderLayout());
-//
-//
-//        JPanel webBrowserPanel = new JPanel(new BorderLayout());
-//        final JWebBrowser webBrowser = new JWebBrowser();
-//        webBrowser.navigate("http://www.baidu.com");
-//        webBrowserPanel.add(webBrowser, BorderLayout.CENTER);
-//        mainPane.add(webBrowserPanel, BorderLayout.CENTER);
-//
-//
-//        mainPane.updateUI();
-//        new Thread(() -> {
-//            if (!NativeInterface.isEventPumpRunning()) {
-//                NativeInterface.runEventPump();
-//            }
-//        }).start();
+        NativeInterface.open();
+        new Thread() {
+            @Override
+            public void run() {
+
+                SwingUtilities.invokeLater(() -> {
+                    mainPane.setLayout(new BorderLayout());
+
+
+                    JPanel webBrowserPanel = new JPanel(new BorderLayout());
+                    webBrowserPanel.setBorder(BorderFactory.createTitledBorder("Native Web Browser component"));
+                    final JWebBrowser webBrowser = new JWebBrowser();
+                    webBrowser.navigate("http://www.4399.com");
+                    webBrowserPanel.add(webBrowser, BorderLayout.CENTER);
+                    mainPane.add(webBrowserPanel, BorderLayout.CENTER);
+
+
+                    mainPane.updateUI();
+                });
+                NativeInterface.runEventPump();
+            }
+        }.start();
+
     }
 
     public JPanel getContent() {
         return mainPane;
-    }
-}
-
-class Testdsf{
-    public static void main(String[] args) {
-        // Initialize Chromium.
-//        Engine engine = Engine.newInstance(HARDWARE_ACCELERATED);
-//
-//// Create a Browser instance.
-//        com.teamdev.jxbrowser.browser.Browser browser = engine.newBrowser();
-//
-//        SwingUtilities.invokeLater(() -> {
-//            JFrame frame = new JFrame("JxBrowser AWT/Swing");
-//            frame.addWindowListener(new WindowAdapter() {
-//                @Override
-//                public void windowClosing(WindowEvent e) {
-//                    // Shutdown Chromium and release allocated resources.
-//                    engine.close();
-//                }
-//            });
-//            // Create and embed Swing BrowserView component to display web content.
-//            frame.add(BrowserView.newInstance(browser));
-//            frame.setSize(1280, 800);
-//            frame.setLocationRelativeTo(null);
-//            frame.setVisible(true);
-//
-//            // Load the required web page.
-//            browser.navigation().loadUrl("https://html5test.com/");
-//        });
     }
 }
